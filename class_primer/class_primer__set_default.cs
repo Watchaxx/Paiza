5d3a6c3b22e182fdaf896d5e14b056e86726d0ba
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-08-19 18:04:44 +0900

    静的メンバ5 デフォルト引数
    https://paiza.jp/works/mondai/class_primer/class_primer__set_default

---
+++ class_primer/class_primer__set_default.cs
@@ -0,0 +1,75 @@
+using System.Linq;
+using static System.Console;
+
+internal class Program
+{
+    class Cus
+    {
+        internal uint Amo { get; set; } = 0;
+
+        internal virtual void Beer() { }
+
+        internal virtual void Order( string s, uint i )
+        {
+            if( StrComp( s, "food" ) || StrComp( s, "softdrink" ) ) {
+                Amo += i;
+            }
+            return;
+        }
+    }
+
+    class Adu : Cus
+    {
+        bool Dis { get; set; } = false;
+
+        internal override void Beer()
+        {
+            Amo += 500;
+            Dis = true;
+            return;
+        }
+
+        internal override void Order( string s, uint i )
+        {
+            if( StrComp( s, "food" ) == true ) {
+                Amo += Dis ? i - 200 : i;
+            } else if( StrComp( s, "softdrink" ) == true ) {
+                Amo += i;
+            } else if( StrComp( s, "alcohol" ) == true ) {
+                Amo += i;
+                Dis = true;
+            }
+            return;
+        }
+    }
+
+    static bool StrComp( string a, string b )
+    {
+        return string.Compare( a, b, System.StringComparison.Ordinal ) == 0;
+    }
+
+    static void Main()
+    {
+        int[] nk = ReadLine().Split().Select( int.Parse ).ToArray();
+        Cus[] a = new Cus[nk[0]];
+
+        SetOut( new System.IO.StreamWriter( OpenStandardOutput() ) { AutoFlush = false } );
+        foreach( int i in Enumerable.Range( 0, nk[0] ) ) {
+            a[i] = int.Parse( ReadLine() ) < 20 ? new Cus() : new Adu();
+        }
+        foreach( int i in Enumerable.Range( 0, nk[1] ) ) {
+            string[] s = ReadLine().Split();
+
+            if( s.Length == 3 ) {
+                a[int.Parse( s[0] ) - 1].Order( s[1], uint.Parse( s[2] ) );
+            } else if( StrComp( s[1], "0" ) == true ) {
+                a[int.Parse( s[0] ) - 1].Beer();
+            }
+        }
+        foreach( Cus c in a ) {
+            WriteLine( c.Amo );
+        }
+        Out.Flush();
+        return;
+    }
+}
