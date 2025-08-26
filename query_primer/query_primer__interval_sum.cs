b1562ae5428b01e623de8d1c2756e5c917da4445
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-08-26 11:17:55 +0900

    平方分割2 区間和
    https://paiza.jp/works/mondai/query_primer/query_primer__interval_sum

---
+++ query_primer/query_primer__interval_sum.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using static System.Console;
+
+internal class Program
+{
+    static void Main()
+    {
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[] a = new int[n[0] + 1];
+
+        SetOut( new System.IO.StreamWriter( OpenStandardOutput() ) { AutoFlush = false } );
+        foreach( int i in Enumerable.Range( 1, n[0] ) ) {
+            a[i] = a[i - 1] + int.Parse( ReadLine() );
+        }
+        foreach( int _ in Enumerable.Range( 0, n[1] ) ) {
+            int[] t = ReadLine().Split().Select( int.Parse ).ToArray();
+
+            WriteLine( a[t[1]] - a[t[0] - 1] );
+        }
+        Out.Flush();
+        return;
+    }
+}
