3fbe8c1a62da0ecab011ecd9ba6e6cd3ce369c37
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-08-28 16:31:31 +0900

    【部分列】最長減少部分列1 最長部分増加列
    https://paiza.jp/works/mondai/dp_primer/dp_primer_lis_step0

---
+++ dp_primer/dp_primer_lis_step0.cs
@@ -0,0 +1,30 @@
+// 実行時間 180ms
+using System;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse( Console.ReadLine() );
+        int o = 1;
+        int[] a = new int[n];
+        int[] p = new int[n];
+
+        foreach( int i in Range( 0, n ) ) {
+            a[i] = int.Parse( Console.ReadLine() );
+        }
+        p[0] = 1;
+        foreach( int i in Range( 1, n - 1 ) ) {
+            p[i] = 1;
+            foreach( int j in Range( 0, i ) ) {
+                if( a[j] < a[i] ) {
+                    p[i] = Math.Max( p[i], p[j] + 1 );
+                    o = Math.Max( o, p[i] );
+                }
+            }
+        }
+        Console.WriteLine( o );
+        return;
+    }
+}
