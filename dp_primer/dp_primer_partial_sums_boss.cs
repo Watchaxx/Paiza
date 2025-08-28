3bdf30528a088c91b3c00b72d82f66ca13dac7b7
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-08-28 17:10:22 +0900

    【部分和】部分和問題 44 【部分和】部分和問題 4
    https://paiza.jp/works/mondai/dp_primer/dp_primer_partial_sums_boss

---
+++ dp_primer/dp_primer_partial_sums_boss.cs
@@ -0,0 +1,27 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int[] nx = ReadLine().Split( ' ' ).Select( int.Parse ).ToArray();
+        int[] a = new int[nx[0]];
+        bool[] p = new bool[nx[1] + 1];
+
+        foreach( int i in Range( 0, nx[0] ) ) {
+            a[i] = int.Parse( ReadLine() );
+        }
+        p[0] = true;
+        foreach( int i in Range( 0, nx[0] ) ) {
+            for( int j = a[i]; j <= nx[1]; j++ ) {
+                if( p[j - a[i]] == true ) {
+                    p[j] = true;
+                }
+            }
+        }
+        WriteLine( p[nx[1]] ? "yes" : "no" );
+        return;
+    }
+}
