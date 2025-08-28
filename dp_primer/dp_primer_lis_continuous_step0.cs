83b001f99f438fa7fd2a3c915b91a3eb8e0345be
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-08-28 16:15:34 +0900

    【連続列】最長減少連続部分列1 最長増加連続部分列
    https://paiza.jp/works/mondai/dp_primer/dp_primer_lis_continuous_step0

---
+++ dp_primer/dp_primer_lis_continuous_step0.cs
@@ -0,0 +1,25 @@
+// 実行時間 90ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse( ReadLine() );
+        byte[] a = new byte[n];
+        int[] p = new int[n];
+        int o = 1;
+
+        foreach( int i in Range( 0, n ) ) {
+            a[i] = byte.Parse( ReadLine() );
+        }
+        p[0] = 1;
+        foreach( int i in Range( 1, n - 1 ) ) {
+            p[i] = a[i - 1] <= a[i] ? p[i - 1] + 1 : 1;
+            o = System.Math.Max( o, p[i] );
+        }
+        WriteLine( o );
+        return;
+    }
+}
