e6abf1dbf49070bedb70a50d4b750a8682d5caee
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-08-29 18:06:06 +0900

    【部分和】部分和問題 44 【部分和】部分和問題 4
    https://paiza.jp/works/mondai/dp_primer/dp_primer_partial_sums_boss

---
+++ dp_primer/dp_primer_partial_sums_boss.py
@@ -0,0 +1,13 @@
+# 実行時間 30ms
+n, x = list(map(int, input().split()))
+a = []
+p = [False for _ in [0] * (x + 1)]
+
+for _ in [0] * n:
+    a.append(int(input()))
+p[0] = True
+for i in range(n):
+    for j in range(a[i], x + 1):
+        if p[j - a[i]] == True:
+            p[j] = True
+print("yes" if p[x] else "no")
