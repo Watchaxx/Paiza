1319e856c7bf0a200f9c8ca31d8e050a5ad047b5
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-02 11:58:58 +0900

    五目並べ1 文字列の出力
    https://paiza.jp/works/mondai/prob60/tic_tac_toe_0

---
+++ prob60/tic_tac_toe_0.cs
@@ -0,0 +1,13 @@
+// 実行時間 10ms
+using static System.Console;
+
+internal class Program
+{
+    static void Main()
+    {
+        for( int _ = 0; _ < 5; _++ ) {
+            WriteLine( ReadLine() );
+        }
+        return;
+    }
+}
