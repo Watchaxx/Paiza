f48f7592ef955c94b49c03dc5a8988ab65048f1a
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-08 14:06:15 +0900

    値の計算5 「値の計算」を解くために：part4
    https://paiza.jp/works/mondai/b_rank_skillcheck_archive/resistor_ohm_calc_04

---
+++ b_rank_skillcheck_archive/resistor_ohm_calc_04.py
@@ -0,0 +1,15 @@
+# 実行時間 10ms
+import math
+
+n = int(input())
+o = 0
+d = {}
+
+for _ in [0] * n:
+    s = input().split()
+
+    d[s[0]] = int(s[1])
+input()
+for i in input():
+    o += 1 / d[i]
+print(math.floor(1 / o))
