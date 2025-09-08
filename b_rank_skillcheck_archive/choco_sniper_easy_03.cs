c33e71b6297ae7111bf5362da52f5b0e2436b202
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-08 18:31:35 +0900

    「地下アイドルの夢」4 「地下アイドルの夢」を解くために：part3
    https://paiza.jp/works/mondai/b_rank_skillcheck_archive/choco_sniper_easy_03

---
+++ b_rank_skillcheck_archive/choco_sniper_easy_03.cs
@@ -0,0 +1,18 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int o = 0;
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+
+        foreach( int _ in Range( 0, n[1] ) ) {
+            o += System.Math.Max( int.Parse( ReadLine() ), 0 );
+        }
+        WriteLine( o );
+        return;
+    }
+}
