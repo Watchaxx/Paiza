1612d9f6261f5856012809e4a3004d0f8744237b
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-08 14:13:13 +0900

    値の計算6 「値の計算」を解くために：part5
    https://paiza.jp/works/mondai/b_rank_skillcheck_archive/resistor_ohm_calc_05

---
+++ b_rank_skillcheck_archive/resistor_ohm_calc_05.cs
@@ -0,0 +1,34 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        decimal o = 0m;
+        int n = int.Parse( ReadLine() );
+        var d = new System.Collections.Generic.Dictionary<char, int>( n );
+
+        foreach( int _ in Range( 0, n ) ) {
+            string[] s = ReadLine().Split();
+
+            d[s[0][0]] = int.Parse( s[1] );
+        }
+        ReadLine();
+        foreach( string s in ReadLine().Split() ) {
+            if( s.Length == 1 ) {
+                o += d[s[0]];
+            } else {
+                decimal t = 0m;
+
+                foreach( char c in s ) {
+                    t += 1m / d[c];
+                }
+                o += 1m / t;
+            }
+        }
+        WriteLine( (int)o );
+        return;
+    }
+}
