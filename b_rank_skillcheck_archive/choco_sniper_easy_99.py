b4c76d272c67abf011c3b8f1aebc37012ab5d4ac
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-08 18:43:00 +0900

    「地下アイドルの夢」5 「地下アイドルの夢」
    https://paiza.jp/works/mondai/b_rank_skillcheck_archive/choco_sniper_easy_99

--- b_rank_skillcheck_archive/choco_sniper_easy_99.py
+++ b_rank_skillcheck_archive/choco_sniper_easy_99.py
@@ -1,18 +1,14 @@
 # 実行時間 170ms
 import sys
 
-def main():
-    r = sys.stdin.readline
-    o = 0
-    n, m = list(map(int, r().split()))
+r = sys.stdin.readline
+o = 0
+n, m = list(map(int, r().split()))
 
-    if n == 0:
-        return o
+if n != 0:
     for _ in [0] * m:
         e = sum(list(map(int, r().split())))
 
         if 0 < e:
             o += e
-    return o
-
-print(main())
+print(o)
