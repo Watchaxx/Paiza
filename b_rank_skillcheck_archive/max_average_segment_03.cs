78ea9a1af5831d5e39eca998e399ba93259756ee
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-10 01:23:40 +0900

    「PV調査」4 「PV調査」を解くために:part3
    https://paiza.jp/works/mondai/b_rank_skillcheck_archive/max_average_segment_03

---
+++ b_rank_skillcheck_archive/max_average_segment_03.cs
@@ -0,0 +1,25 @@
+// 実行時間 120ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int[] n = ReadLine().Split( ' ' ).Select( int.Parse ).ToArray();
+        int[] a = ReadLine().Split( ' ' ).Select( int.Parse ).ToArray();
+        int[] b = new int[n[0] - n[1] + 1];
+        int s = a.Take( n[1] ).Sum();
+        var l = new System.Collections.Generic.List<int>( a.Skip( n[1] ) );
+
+        foreach( int i in Range( 0, l.Count ) ) {
+            l[i] -= a[i];
+            b[i + 1] = b[i] + l[i];
+        }
+        foreach( int i in Range( 0, b.Length ) ) {
+            b[i] += s;
+        }
+        WriteLine( string.Join( " ", b ) );
+        return;
+    }
+}
