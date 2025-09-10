b58575dc2d02f7b1ec753a2b57626210f03a5a23
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-10 16:05:00 +0900

    ビームの反射4 「ビームの反射」を解くために:part3
    https://paiza.jp/works/mondai/b_rank_skillcheck_archive/beam_reflecting_03

---
+++ b_rank_skillcheck_archive/beam_reflecting_03.cs
@@ -0,0 +1,49 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int dx = 1;
+        int dy = 0;
+        int o = 0;
+        int px = -1;
+        int py = 0;
+        int[] hw = ReadLine().Split().Select( int.Parse ).ToArray();
+        string[] s = new string[hw[0]];
+
+        foreach( int i in Range( 0, hw[0] ) ) {
+            s[i] = ReadLine();
+        }
+        while( true ) {
+            px += dx;
+            py += dy;
+            if( px < 0 || hw[1] <= px || py < 0 || hw[0] <= py ) {
+                break;
+            }
+            o++;
+            switch( s[py][px] ) {
+            case 'R':
+                dx = 1;
+                dy = 0;
+                break;
+            case 'L':
+                dx = -1;
+                dy = 0;
+                break;
+            case 'U':
+                dx = 0;
+                dy = -1;
+                break;
+            case 'D':
+                dx = 0;
+                dy = 1;
+                break;
+            }
+        }
+        WriteLine( o );
+        return;
+    }
+}
