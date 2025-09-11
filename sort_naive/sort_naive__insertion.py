887818f1c5a0cd787273e72dd2b18361689312ce
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-12 01:36:15 +0900

    挿入ソート
    https://paiza.jp/works/mondai/sort_naive/sort_naive__insertion

---
+++ sort_naive/sort_naive__insertion.py
@@ -0,0 +1,13 @@
+# 実行時間 190ms
+n = int(input())
+a = list(map(int, input().split()))
+
+for i in range(1, n):
+    x = a[i]
+    j = i - 1
+
+    while 0 <= j and x < a[j]:
+        a[j + 1] = a[j]
+        j -= 1
+    a[j + 1] = x
+    print(" ".join(map(str, a)))
