a7e9357d6bb643c1ed2cdbcd06baacd93c2055f5
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-12 10:39:29 +0900

    バブルソート
    https://paiza.jp/works/mondai/sort_naive/sort_naive__bubble

---
+++ sort_naive/sort_naive__bubble.py
@@ -0,0 +1,9 @@
+# 実行時間 410ms
+n = int(input())
+a = list(map(int, input().split()))
+
+for i in range(n - 1):
+    for j in range(n - 1, i, -1):
+        if a[j] < a[j - 1]:
+            a[j], a[j - 1] = a[j - 1], a[j]
+    print(*a)
