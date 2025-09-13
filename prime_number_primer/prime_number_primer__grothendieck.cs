28c48ca65dc08a4ee925305191a858d7b56293a1
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-13 16:55:20 +0900

    素数大学1 グロタンディーク素数
    https://paiza.jp/works/mondai/prime_number_primer/prime_number_primer__grothendieck

--- prime_number_primer/prime_number_primer__grothendieck.cs
+++ prime_number_primer/prime_number_primer__grothendieck.cs
@@ -1,11 +1,9 @@
 // 実行時間 10ms
-using static System.Console;
-
 internal class Program
 {
     static void Main()
     {
-        WriteLine( "NO" );
+        System.Console.WriteLine( "NO" );
         return;
     }
 }
