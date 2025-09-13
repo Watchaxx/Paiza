17859fd2d7113a9cff0ff8e39e64f235850bafbb
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-13 16:30:30 +0900

    Delete prime_number_primer directory

--- prime_number_primer/prime_number_primer__fermat_test.py
+++
@@ -1,9 +0,0 @@
-# 実行時間 10ms
-def main():
-    n = int(input())
-
-    if n % 2 == 0:
-        return False
-    return pow(2, n - 1) % n == 1
-
-print("YES" if main() else "NO")
--- prime_number_primer/prime_number_primer__grothendieck.cs
+++
@@ -1,8 +0,0 @@
-internal class Program
-{
-    static void Main()
-    {
-        System.Console.WriteLine( "NO" );
-        return;
-    }
-}
