bf2170abeac83e55f509a54dd97d02afb7579642
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-17 19:40:19 +0900

    三角形を作る2 木の入力の受け取り（隣接リスト）
    https://paiza.jp/works/mondai/tree_primer/tree_primer__adjacency_list_input

---
+++ tree_primer/tree_primer__adjacency_list_input.cs
@@ -0,0 +1,27 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+using Lst = System.Collections.Generic.List<byte>;
+
+internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse( ReadLine() );
+        var g = new Lst[n];
+
+        foreach( int i in Range( 0, n ) ) {
+            g[i] = new Lst( n );
+        }
+        foreach( int _ in Range( 0, n - 1 ) ) {
+            byte[] a = ReadLine().Split().Select( byte.Parse ).ToArray();
+
+            g[a[0] - 1].Add( a[1] );
+            g[a[1] - 1].Add( a[0] );
+        }
+        foreach( Lst l in g ) {
+            WriteLine( string.Join( " ", l.OrderBy( x => x ) ) );
+        }
+        return;
+    }
+}
