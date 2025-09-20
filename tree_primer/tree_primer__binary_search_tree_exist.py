c91549f090850836dc4b4ad5926c31acb54d7bad
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-20 13:53:30 +0900

    二分探索木での値の検索6 二分探索木での値の検索
    https://paiza.jp/works/mondai/tree_primer/tree_primer__binary_search_tree_exist

---
+++ tree_primer/tree_primer__binary_search_tree_exist.py
@@ -0,0 +1,11 @@
+# 実行時間 10ms
+n, k, r = list(map(int, input().split()))
+s = set()
+
+for _ in [0] * (n - 1):
+    a, b = list(map(int, input().split()))
+
+    s.add(a)
+    s.add(b)
+for _ in [0] * k:
+    print("Yes" if int(input()) in s else "No")
