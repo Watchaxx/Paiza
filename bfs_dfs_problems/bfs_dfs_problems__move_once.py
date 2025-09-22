91d01b5e0ea253645786e596754439f64e413c69
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-22 12:03:25 +0900

    迷路1 1 マスの移動
    https://paiza.jp/works/mondai/bfs_dfs_problems/bfs_dfs_problems__move_once

---
+++ bfs_dfs_problems/bfs_dfs_problems__move_once.py
@@ -0,0 +1,16 @@
+# 実行時間 10ms
+h, w = list(map(int, input().split()))
+y, x = list(map(int, input().split()))
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+g = [["." for _ in [0] * w] for _ in [0] * h]
+
+g[y][x] = "*"
+for i in range(4):
+    a = x + dx[i]
+    b = y + dy[i]
+
+    if 0 <= b and b < h and 0 <= a and a < w:
+        g[b][a] = "*"
+for c in g:
+    print("".join(c))
