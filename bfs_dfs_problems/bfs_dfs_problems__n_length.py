e4baff53cf4dea7834c20a4dd2eb6c6f67b68ae5
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-26 15:21:44 +0900

    2 頂点間の最短経路3 木における幅優先探索での探索
    https://paiza.jp/works/mondai/bfs_dfs_problems/bfs_dfs_problems__n_length

---
+++ bfs_dfs_problems/bfs_dfs_problems__n_length.py
@@ -0,0 +1,28 @@
+# 実行時間 390ms
+from collections import deque
+
+n, x, l = list(map(int, input().split()))
+d = [-1] * n
+g = [[] for _ in [0] * n]
+q = deque()
+
+x -= 1
+d[x] = 0
+for _ in [0] * (n - 1):
+    a, b = list(map(int, input().split()))
+
+    a -= 1
+    b -= 1
+    g[a].append(b)
+    g[b].append(a)
+q.append(x)
+while 0 < len(q):
+    s = q.popleft()
+
+    for i in g[s]:
+        if d[i] == -1:
+            d[i] = d[s] + 1
+            q.append(i)
+for i, j in enumerate(d):
+    if j == l:
+        print(i + 1)
