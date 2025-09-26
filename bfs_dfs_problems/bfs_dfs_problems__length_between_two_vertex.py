15cd8d2b5102744adaa7a660ab80d676f124c837
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-26 15:29:49 +0900

    2 頂点間の最短経路4 木の 2 頂点間の距離
    https://paiza.jp/works/mondai/bfs_dfs_problems/bfs_dfs_problems__length_between_two_vertex

---
+++ bfs_dfs_problems/bfs_dfs_problems__length_between_two_vertex.py
@@ -0,0 +1,26 @@
+# 実行時間 350ms
+from collections import deque
+
+n, x, y = list(map(int, input().split()))
+d = [-1] * n
+g = [[] for _ in [0] * n]
+q = deque()
+
+x -= 1
+d[x] = 0
+for _ in [0] * (n - 1):
+    a, b = list(map(int, input().split()))
+
+    a -= 1
+    b -= 1
+    g[a].append(b)
+    g[b].append(a)
+q.append(x)
+while 0 < len(q):
+    s = q.popleft()
+
+    for i in g[s]:
+        if d[i] == -1:
+            d[i] = d[s] + 1
+            q.append(i)
+print(d[y - 1])
