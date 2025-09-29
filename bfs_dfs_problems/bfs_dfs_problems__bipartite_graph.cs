e6753d864045252921374d89050f018aad56ee0b
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-09-29 14:43:14 +0900

    連結成分の個数2 二部グラフ判定
    https://paiza.jp/works/mondai/bfs_dfs_problems/bfs_dfs_problems__bipartite_graph

---
+++ bfs_dfs_problems/bfs_dfs_problems__bipartite_graph.cs
@@ -0,0 +1,44 @@
+// 実行時間 30ms
+using static System.Console;
+using static System.Linq.Enumerable;
+using Lst = System.Collections.Generic.List<int>;
+
+internal class Program
+{
+    static bool b = true;
+
+    static void Main()
+    {
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[] c = new int[n[0]];
+        Lst[] g = new Lst[n[0]];
+
+        foreach( int i in Range( 0, n[0] ) ) {
+            g[i] = new Lst();
+        }
+        foreach( int _ in Range( 0, n[1] ) ) {
+            int[] a = ReadLine().Split().Select( x => int.Parse( x ) - 1 ).ToArray();
+
+            g[a[0]].Add( a[1] );
+            g[a[1]].Add( a[0] );
+        }
+        foreach( int i in Range( 0, n[0] ).Where( x => c[x] == 0 ) ) {
+            Dfs( i, 1, c, g );
+        }
+        WriteLine( b ? "Yes" : "No" );
+        return;
+    }
+
+    static void Dfs( int v, int l, int[] c, Lst[] g )
+    {
+        c[v] = l;
+        foreach( int i in g[v] ) {
+            if( c[i] == 0 ) {
+                Dfs( i, -l, c, g );
+            } else if( c[i] == c[v] ) {
+                b = false;
+            }
+        }
+        return;
+    }
+}
