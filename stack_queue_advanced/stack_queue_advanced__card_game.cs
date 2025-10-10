223ae6b9da80836e4bdb7fd2c6ef7ac8d7b2fdce
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-10-10 19:59:09 +0900

    カードゲーム5 カードゲーム
    https://paiza.jp/works/mondai/stack_queue_advanced/stack_queue_advanced__card_game

--- stack_queue_advanced/stack_queue_advanced__card_game.cs
+++ stack_queue_advanced/stack_queue_advanced__card_game.cs
@@ -8,31 +8,28 @@ internal class Program
     static void Main()
     {
         int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        var ym = new Queue<bool>( 40 );
         var ex = new Stack<bool>();
         var gv = new Stack<bool>();
-        var ym = new Queue<bool>( 40 );
 
         foreach( int i in Range( 0, 40 ) ) {
             ym.Enqueue( i == n[0] - 1 );
         }
         ReadLine();
-        foreach( int _ in Range( 0, 5 ) ) {
-            if( ym.Dequeue() == true ) {
-                WriteLine( 1 );
-                return;
-            }
+        foreach( int _ in Range( 0, 5 ).Where( x => ym.Dequeue() == true ) ) {
+            WriteLine( 1 );
+            return;
         }
+
         foreach( int i in Range( 2, n[1] - 1 ) ) {
             string[] e = ReadLine().Split();
             int x = int.Parse( e[1] );
 
             switch( e[0] ) {
             case "draw":
-                foreach( int j in Range( 0, x ) ) {
-                    if( ym.Dequeue() == true ) {
-                        WriteLine( i );
-                        return;
-                    }
+                foreach( int _ in Range( 0, x ).Where( j => ym.Dequeue() == true ) ) {
+                    WriteLine( i );
+                    return;
                 }
                 break;
             case "discard":
