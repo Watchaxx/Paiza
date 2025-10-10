989fffb9b50f17a45446fa10f232f8de6c46682f
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-10-10 11:02:00 +0900

    【いもす法の練習問題】復習問題 その 1
    https://paiza.jp/works/mondai/prefix_sum_problems_advanced/prefix_sum_problems_advanced__imos_review_step1

---
+++ prefix_sum_problems_advanced/prefix_sum_problems_advanced__imos_review_step1.cs
@@ -0,0 +1,27 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int o = 0;
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[] a = new int[n[0] + 1];
+        int[] b = new int[n[0] + 1];
+
+        foreach( int _ in Range( 0, n[1] ) ) {
+            int[] t = ReadLine().Split().Select( int.Parse ).ToArray();
+
+            a[t[0] - 1]++;
+            a[t[1]]--;
+        }
+        foreach( int i in Range( 0, n[0] ) ) {
+            b[i + 1] = b[i] + a[i];
+            o = System.Math.Max( o, b[i + 1] );
+        }
+        WriteLine( o );
+        return;
+    }
+}
