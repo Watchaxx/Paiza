222d53ea0eb7a97365e9552ee1b2d3f2072e271d
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-10-10 11:08:34 +0900

    【しゃくとり法の練習問題】 復習問題 その 2
    https://paiza.jp/works/mondai/prefix_sum_problems_advanced/prefix_sum_problems_advanced__syakutori_review_step2

---
+++ prefix_sum_problems_advanced/prefix_sum_problems_advanced__syakutori_review_step2.cs
@@ -0,0 +1,30 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int c = 0;
+        int r = 0;
+        int s = 0;
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[] a = ReadLine().Split().Select( int.Parse ).ToArray();
+
+        foreach( int l in Range( 0, n[0] ) ) {
+            while( r < n[0] && s + a[r] <= n[1] ) {
+                s += a[r];
+                r++;
+            }
+            c = System.Math.Max( c, r - l );
+            if( l == r ) {
+                r++;
+            } else {
+                s -= a[l];
+            }
+        }
+        WriteLine( c );
+        return;
+    }
+}
