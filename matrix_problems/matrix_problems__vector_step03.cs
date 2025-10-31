49a4fdfd9c9005f861c67049998c8179b726e120
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-10-31 12:10:10 +0900

    ベクトルの応用 23 ベクトルの内積
    https://paiza.jp/works/mondai/matrix_problems/matrix_problems__vector_step03

---
+++ matrix_problems/matrix_problems__vector_step03.cs
@@ -0,0 +1,21 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse( ReadLine() );
+        int o = 0;
+        int[] a = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[] b = ReadLine().Split().Select( int.Parse ).ToArray();
+
+        foreach( int i in Range( 0, n ) ) {
+            a[i] *= b[i];
+            o += a[i];
+        }
+        WriteLine( o );
+        return;
+    }
+}
