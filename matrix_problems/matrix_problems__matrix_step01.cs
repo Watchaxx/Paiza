e82fed38a11a184f37a46cbc1158e281d660a3e1
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-10-31 14:17:14 +0900

    ハミング符号 31 行列の足し算
    https://paiza.jp/works/mondai/matrix_problems/matrix_problems__matrix_step01

---
+++ matrix_problems/matrix_problems__matrix_step01.cs
@@ -0,0 +1,27 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int[] hw = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[][] a = new int[hw[0]][];
+
+        foreach( int i in Range( 0, hw[0] ) ) {
+            a[i] = ReadLine().Split().Select( int.Parse ).ToArray();
+        }
+        foreach( int i in Range( 0, hw[0] ) ) {
+            int[] t = ReadLine().Split().Select( int.Parse ).ToArray();
+
+            foreach( int j in Range( 0, hw[1] ) ) {
+                a[i][j] += t[j];
+            }
+        }
+        foreach( int[] i in a ) {
+            WriteLine( string.Join( " ", i ) );
+        }
+        return;
+    }
+}
