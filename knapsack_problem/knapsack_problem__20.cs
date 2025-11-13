dc86de2fe9382ea6b5c9e0d8828044d4df3c19e4
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-11-14 01:45:20 +0900

    個数制限なしナップサック問題 1
    https://paiza.jp/works/mondai/knapsack_problem/knapsack_problem__20

---
+++ knapsack_problem/knapsack_problem__20.cs
@@ -0,0 +1,30 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[] v = new int[n[0]];
+        int[] w = new int[n[0]];
+        int[,] d = new int[n[0] + 1, n[1] + 1];
+
+        foreach( int i in Range( 0, n[0] ) ) {
+            int[] t = ReadLine().Split().Select( int.Parse ).ToArray();
+
+            w[i] = t[0];
+            v[i] = t[1];
+        }
+        foreach( int i in Range( 0, n[0] ) ) {
+            foreach( int j in Range( 0, n[1] + 1 ) ) {
+                for( int k = 0; k * w[i] <= j; k++ ) {
+                    d[i + 1, j] = System.Math.Max( d[i + 1, j], d[i, j - k * w[i]] + k * v[i] );
+                }
+            }
+        }
+        WriteLine( d[n[0], n[1]] );
+        return;
+    }
+}
