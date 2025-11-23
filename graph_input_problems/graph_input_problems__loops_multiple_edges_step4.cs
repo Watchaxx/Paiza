ab9c0a03590653bee3f12471cb2110fa1a1ce881
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-11-23 19:49:29 +0900

    多重辺・有向グラフ (辺入力)4 多重辺・有向グラフ
    https://paiza.jp/works/mondai/graph_input_problems/graph_input_problems__loops_multiple_edges_step4

---
+++ graph_input_problems/graph_input_problems__loops_multiple_edges_step4.cs
@@ -0,0 +1,28 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse( ReadLine() );
+        int[][] g = new int[n][];
+        var l = new System.Collections.Generic.List<(int, int)>();
+
+        foreach( int i in Range( 0, n ) ) {
+            g[i] = ReadLine().Split().Select( int.Parse ).ToArray();
+        }
+        foreach( int i in Range( 0, n ) ) {
+            foreach( int j in Range( i + 1, n - i - 1 ).Where( x => 1 < g[i][x] + g[x][i] ) ) {
+                l.Add( (i + 1, j + 1) );
+            }
+        }
+        WriteLine( l.Count );
+        if( 0 < l.Count ) {
+            WriteLine( string.Join( System.Environment.NewLine,
+                l.Select( x => $"{x.Item1} {x.Item2}" ) ) );
+        }
+        return;
+    }
+}
