8b4d1cf0c996e098b5db951d7c4ec4232792e914
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-11-23 22:31:45 +0900

    しりとり1 頂点の出現回数・無向グラフ
    https://paiza.jp/works/mondai/graph_input_problems/graph_input_problems__degree_euler_graph_step1

---
+++ graph_input_problems/graph_input_problems__degree_euler_graph_step1.cs
@@ -0,0 +1,25 @@
+// 実行時間 20ms
+using System.Collections.Generic;
+using static System.Console;
+using static System.Linq.Enumerable;
+
+class Program
+{
+    static void Main()
+    {
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        var l = new List<int>[n[0] + 1];
+
+        foreach( int i in Range( 1, n[0] ) ) {
+            l[i] = new List<int>();
+        }
+        foreach( int _ in Range( 0, n[1] ) ) {
+            int[] t = ReadLine().Split().Select( int.Parse ).ToArray();
+
+            l[t[0]].Add( t[1] );
+            l[t[1]].Add( t[0] );
+        }
+        WriteLine( l[n[2]].Count );
+        return;
+    }
+}
