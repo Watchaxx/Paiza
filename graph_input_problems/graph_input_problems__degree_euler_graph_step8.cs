1ed9208d749c517b3d68de5d7c54897e75e17b75
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-11-24 11:15:25 +0900

    しりとり8 準オイラーグラフ・有向グラフ
    https://paiza.jp/works/mondai/graph_input_problems/graph_input_problems__degree_euler_graph_step8

---
+++ graph_input_problems/graph_input_problems__degree_euler_graph_step8.cs
@@ -0,0 +1,36 @@
+// 実行時間 30ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+class Program
+{
+    static void Main()
+    {
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[] li = new int[n[0]];
+        int[] lo = new int[n[0]];
+
+        foreach( int _ in Range( 0, n[1] ) ) {
+            int[] t = ReadLine().Split().Select( x => int.Parse( x ) - 1 ).ToArray();
+
+            li[t[1]]++;
+            lo[t[0]]++;
+        }
+        foreach( int _ in Range( 0, n[0] ).Where( x => li[x] != lo[x] ) ) {
+            goto Nx;
+        }
+        WriteLine( 1 );
+        return;
+    Nx:
+        if( Range( 0, n[0] ).Where( x => li[x] == lo[x] + 1 ).Count() == 1 ) {
+            if( Range( 0, n[0] ).Where( x => li[x] + 1 == lo[x] ).Count() == 1 ) {
+                if( Range( 0, n[0] ).Where( x => li[x] == lo[x] ).Count() == n[0] - 2 ) {
+                    WriteLine( 1 );
+                    return;
+                }
+            }
+        }
+        WriteLine( 0 );
+        return;
+    }
+}
