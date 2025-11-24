c13c9a07535c28140e58c6331653cfb23ad1c8f3
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-11-24 18:59:09 +0900

    パスの通れない頂点1 隣接頂点の出力
    https://paiza.jp/works/mondai/graph_dfs_problems/graph_dfs__path_one_step1

---
+++ graph_dfs_problems/graph_dfs__path_one_step1.cs
@@ -0,0 +1,23 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+class Program
+{
+    static void Main()
+    {
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+
+        foreach( int i in Range( 1, n[0] ) ) {
+            if( i != n[1] ) {
+                ReadLine();
+                ReadLine();
+            } else {
+                ReadLine();
+                WriteLine( ReadLine().Split().Select( int.Parse ).Max() );
+                break;
+            }
+        }
+        return;
+    }
+}
