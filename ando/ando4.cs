b8b49f78706715e6ab92ed3d0f99deabd30c481b
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-11-25 22:05:22 +0900

    「ショートヘアセット」ゲットチャレンジ！

---
+++ ando/ando4.cs
@@ -0,0 +1,11 @@
+// 実行時間 10ms
+using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        WriteLine( int.Parse( ReadLine() ) + int.Parse( ReadLine() ) );
+        return;
+    }
+}
