7bd5609391756455f99c0cf917c30f32ab3e4dd9
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-11-25 20:46:30 +0900

    「つり目セット」ゲットチャレンジ！

---
+++ ando/ando2.cs
@@ -0,0 +1,14 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+class Program
+{
+    static void Main()
+    {
+        foreach( int _ in Range( 0, int.Parse( ReadLine() ) ) ) {
+            Write( "Ann" );
+        }
+        return;
+    }
+}
