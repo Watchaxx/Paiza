1df09b74cc11af488e4bc22501635fee93a6f4a2
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-11-27 22:51:30 +0900

    トレイルの通れない頂点 21 グラフのトレイル 2
    https://paiza.jp/works/mondai/graph_dfs_problems/graph_dfs__trail_all_step1

---
+++ graph_dfs_problems/graph_dfs__trail_all_step1.cs
@@ -0,0 +1,48 @@
+// 実行時間 30ms
+using System.Collections.Generic;
+using static System.Console;
+using static System.Linq.Enumerable;
+
+class Program
+{
+    static int[] n;
+    static int[][] a;
+    static List<int[]> o = new List<int[]>();
+
+    static void Main()
+    {
+        n = ReadLine().Split().Select( int.Parse ).ToArray();
+        a = new int[n[0] + 1][];
+        foreach( int i in Range( 1, n[0] ) ) {
+            ReadLine();
+            a[i] = ReadLine().Split().Select( int.Parse ).ToArray();
+        }
+        Dfs( n[1], n[2], new List<int>() { n[1] }, new List<(int, int)>() );
+        WriteLine( o.Count );
+        if( 0 < o.Count ) {
+            WriteLine( string.Join( System.Environment.NewLine,
+                Range( 0, o.Count ).Select( x => string.Join( " ", o[x] ) ) ) );
+        }
+        return;
+    }
+
+    static void Dfs( int v, int k, List<int> t, List<(int, int)> e )
+    {
+        if( k == 0 ) {
+            o.Add( t.ToArray() );
+            return;
+        }
+        foreach( int i in a[v] ) {
+            var f = (new[] { i, v }.Min(), new[] { i, v }.Max());
+
+            if( e.Contains( f ) != true ) {
+                t.Add( i );
+                e.Add( f );
+                Dfs( i, k - 1, t, e );
+                t.RemoveAt( t.Count - 1 );
+                e.RemoveAt( e.Count - 1 );
+            }
+        }
+        return;
+    }
+}
