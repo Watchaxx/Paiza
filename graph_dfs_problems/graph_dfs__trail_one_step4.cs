832415efa4384906872d752f8454bf3c3ce527eb
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-11-27 20:39:39 +0900

    トレイルの通れない頂点4 トレイルの経由地
    https://paiza.jp/works/mondai/graph_dfs_problems/graph_dfs__trail_one_step4

---
+++ graph_dfs_problems/graph_dfs__trail_one_step4.cs
@@ -0,0 +1,43 @@
+// 実行時間 1080ms
+using System.Collections.Generic;
+using static System.Console;
+using static System.Linq.Enumerable;
+
+class Program
+{
+    static int[] n;
+    static int[][] a;
+    static List<int> o = new List<int>();
+
+    static void Main()
+    {
+        n = ReadLine().Split().Select( int.Parse ).ToArray();
+        a = new int[n[0] + 1][];
+        foreach( int i in Range( 1, n[0] ) ) {
+            ReadLine();
+            a[i] = ReadLine().Split().Select( int.Parse ).ToArray();
+        }
+        Dfs( n[1], new List<int>() { n[1] }, new List<(int, int)>() );
+        WriteLine( 0 < o.Count ? string.Join( " ", o ) : "-1" );
+        return;
+    }
+
+    static void Dfs( int v, List<int> t, List<(int, int)> e )
+    {
+        foreach( int i in a[v] ) {
+            var f = (new[] { i, v }.Min(), new[] { i, v }.Max());
+
+            if( e.Contains( f ) != true ) {
+                t.Add( i );
+                e.Add( f );
+                if( i == n[2] && o.Count < t.Count && t.Contains( n[3] ) == true ) {
+                    o = new List<int>( t );
+                }
+                Dfs( i, t, e );
+                t.RemoveAt( t.Count - 1 );
+                e.RemoveAt( e.Count - 1 );
+            }
+        }
+        return;
+    }
+}
