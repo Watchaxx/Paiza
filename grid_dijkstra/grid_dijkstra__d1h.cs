a8a67a20ff1ba259827f826927309cd20e974a38
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-12-01 11:12:02 +0900

    問題1: 幅優先探索 - 迷路 (hard)
    https://paiza.jp/works/mondai/grid_dijkstra/grid_dijkstra__d1h

---
+++ grid_dijkstra/grid_dijkstra__d1h.cs
@@ -0,0 +1,43 @@
+// 実行時間 90ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+class Program
+{
+    static void Main()
+    {
+        int[] dx = { -1, 1, 0, 0 };
+        int[] dy = { 0, 0, -1, 1 };
+        int[] hw = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[][] d = new int[hw[0]][];
+        bool[][] t = new bool[hw[0]][];
+        var q = new System.Collections.Generic.Queue<(int, int)>();
+
+        foreach( int i in Range( 0, hw[0] ) ) {
+            d[i] = Repeat( -1, hw[1] ).ToArray();
+            t[i] = ReadLine().Split().Select( ToBool ).ToArray();
+        }
+        d[0][0] = 1;
+        q.Enqueue( (0, 0) );
+        while( 0 < q.Count ) {
+            var p = q.Dequeue();
+
+            foreach( int i in Range( 0, 4 ) ) {
+                int x = p.Item1 + dx[i];
+                int y = p.Item2 + dy[i];
+
+                if( 0 <= x && x < hw[0] && 0 <= y && y < hw[1] && t[x][y] == true && d[x][y] == -1 ) {
+                    d[x][y] = d[p.Item1][p.Item2] + 1;
+                    q.Enqueue( (x, y) );
+                }
+            }
+        }
+        WriteLine( d[hw[0] - 1][hw[1] - 1] );
+        return;
+    }
+
+    static bool ToBool( string s )
+    {
+        return s == "0";
+    }
+}
