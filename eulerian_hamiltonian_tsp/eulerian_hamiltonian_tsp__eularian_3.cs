d277f67cfe18de20c8b8946de538f6c68cbe23c4
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-12-11 02:00:10 +0900

    2 つのオイラーグラフで完全グラフをつくる03 準オイラーグラフの判定（無向グラフ）
    https://paiza.jp/works/mondai/eulerian_hamiltonian_tsp/eulerian_hamiltonian_tsp__eularian_3

---
+++ eulerian_hamiltonian_tsp/eulerian_hamiltonian_tsp__eularian_3.cs
@@ -0,0 +1,50 @@
+// 実行時間 230ms
+using static System.Console;
+using static System.Linq.Enumerable;
+using Lst = System.Collections.Generic.List<int>;
+
+class Program
+{
+    static bool[] v;
+    static Lst[] g;
+
+    static void Main()
+    {
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[] d = new int[n[0]];
+
+        v = new bool[n[0]];
+        g = new Lst[n[0]];
+        foreach( int i in Range( 0, n[0] ) ) {
+            g[i] = new Lst();
+        }
+        foreach( int _ in Range( 0, n[1] ) ) {
+            int[] a = ReadLine().Split().Select( x => int.Parse( x ) - 1 ).ToArray();
+
+            g[a[0]].Add( a[1] );
+            g[a[1]].Add( a[0] );
+        }
+        Dfs( 0 );
+        if( v.Any( x => !x ) ) {
+            WriteLine( "No" );
+            return;
+        }
+        foreach( int i in Range( 0, n[0] ) ) {
+            d[i] = g[i].Count;
+        }
+
+        int o = d.Count( x => ( x & 1 ) != 0 );
+
+        WriteLine( o == 0 || o == 2 ? "Yes" : "No" );
+        return;
+    }
+
+    static void Dfs( int c )
+    {
+        v[c] = true;
+        foreach( int i in g[c].Where( x => v[x] != true ) ) {
+            Dfs( i );
+        }
+        return;
+    }
+}
