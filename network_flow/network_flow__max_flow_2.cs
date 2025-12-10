3e47eb40facfc4164c9f6aad2dc7a9d4f6751e08
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-12-10 16:47:41 +0900

    最大流問題 2
    https://paiza.jp/works/mondai/network_flow/network_flow__max_flow_2

---
+++ network_flow/network_flow__max_flow_2.cs
@@ -0,0 +1,46 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+class Program
+{
+    static bool[] visited;
+    static int[][] capacity;
+
+    static void Main()
+    {
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+
+        visited = new bool[n[0]];
+        capacity = new int[n[0]][];
+        foreach( int i in Range( 0, n[0] ) ) {
+            capacity[i] = new int[n[0]];
+        }
+        foreach( int _ in Range( 0, n[1] ) ) {
+            int[] a = ReadLine().Split().Select( x => int.Parse( x ) - 1 ).ToArray();
+
+            capacity[a[0]][a[1]] = a[2] + 1;
+        }
+        WriteLine( PushFlow( n[0], n[2] - 1, n[3] - 1, 100 ) );
+        WriteLine( string.Join( System.Environment.NewLine,
+            capacity.Select( x => string.Join( " ", x ) ) ) );
+        return;
+    }
+
+    static int PushFlow( int n, int v, int t, int f )
+    {
+        if( v == t ) {
+            return f;
+        }
+        visited[v] = true;
+        foreach( int i in Range( 0, n ).Where( x => visited[x] == false && 0 < capacity[v][x] ) ) {
+            int f2 = PushFlow( n, i, t, new[] { f, capacity[v][i] }.Min() );
+
+            if( 0 < f2 ) {
+                capacity[v][i] -= f2;
+                return f2;
+            }
+        }
+        return 0;
+    }
+}
