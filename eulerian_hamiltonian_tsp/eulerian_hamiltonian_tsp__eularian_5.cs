7f6926576e1c54b4572356e216c8616f6f94a396
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-12-11 15:37:30 +0900

    2 つのオイラーグラフで完全グラフをつくる05 準オイラーグラフの判定（有向グラフ）
    https://paiza.jp/works/mondai/eulerian_hamiltonian_tsp/eulerian_hamiltonian_tsp__eularian_5

---
+++ eulerian_hamiltonian_tsp/eulerian_hamiltonian_tsp__eularian_5.cs
@@ -0,0 +1,72 @@
+// 実行時間 240ms
+using static System.Console;
+using static System.Linq.Enumerable;
+using Lst = System.Collections.Generic.List<int>;
+
+class Program
+{
+    static bool[] V;
+    static Lst[] G;
+
+    static void Main()
+    {
+        int e = 0;
+        int m = 0;
+        int s = 0;
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[] d = new int[n[0]];
+        int[] dI = new int[n[0]];
+        int[] dO = new int[n[0]];
+        Lst[] g = new Lst[n[0]];
+
+        V = new bool[n[0]];
+        G = new Lst[n[0]];
+        foreach( int i in Range( 0, n[0] ) ) {
+            G[i] = new Lst();
+            g[i] = new Lst();
+        }
+        foreach( int _ in Range( 0, n[1] ) ) {
+            int[] a = ReadLine().Split().Select( x => int.Parse( x ) - 1 ).ToArray();
+
+            G[a[0]].Add( a[1] );
+        }
+        foreach( int i in Range( 0, n[0] ) ) {
+            foreach( int j in G[i] ) {
+                g[i].Add( j );
+                g[j].Add( i );
+            }
+        }
+        Dfs( 0, g );
+        if( V.Any( x => !x ) ) {
+            WriteLine( "No" );
+            return;
+        }
+        foreach( int i in Range( 0, n[0] ) ) {
+            foreach( int j in G[i] ) {
+                dI[j]++;
+                dO[i]++;
+            }
+        }
+        foreach( int i in Range( 0, n[0] ) ) {
+            if( dI[i] == dO[i] ) {
+                m++;
+            } else if( dI[i] == dO[i] + 1 ) {
+                s++;
+            } else if( dI[i] + 1 == dO[i] ) {
+                e++;
+            }
+        }
+        WriteLine( ( s == 0 && m == n[0] && e == 0 )
+            || ( s == 1 && m == n[0] - 2 && e == 1 ) ? "Yes" : "No" );
+        return;
+    }
+
+    static void Dfs( int c, Lst[] g )
+    {
+        V[c] = true;
+        foreach( int i in g[c].Where( x => V[x] != true ) ) {
+            Dfs( i, g );
+        }
+        return;
+    }
+}
