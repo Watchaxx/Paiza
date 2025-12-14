c9994ff89a2a28f50ecf9705c0da4377b3284294
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-12-14 21:29:49 +0900

    閉路を n 個増やす4 ハミルトン路の個数
    https://paiza.jp/works/mondai/eulerian_hamiltonian_tsp/eulerian_hamiltonian_tsp__hamiltonian_4

---
+++ eulerian_hamiltonian_tsp/eulerian_hamiltonian_tsp__hamiltonian_4.cs
@@ -0,0 +1,61 @@
+// 実行時間 190ms
+using System.Collections.Generic;
+using static System.Console;
+using static System.Linq.Enumerable;
+
+class Program
+{
+    static void Main()
+    {
+        int o = 0;
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[] v = new int[n[0]];
+        bool[][] g = new bool[n[0]][];
+
+        foreach( int i in Range( 0, n[0] ) ) {
+            g[i] = new bool[n[0]];
+            v[i] = i;
+        }
+        foreach( int _ in Range( 0, n[1] ) ) {
+            int[] a = ReadLine().Split().Select( x => int.Parse( x ) - 1 ).ToArray();
+
+            g[a[0]][a[1]] = true;
+        }
+        foreach( IEnumerable<int> p in Permutations( v ) ) {
+            bool b = true;
+            int[] q = p.ToArray();
+
+            foreach( int i in Range( 0, n[0] - 1 ).Where( x => g[q[x]][q[x + 1]] != true ) ) {
+                b = false;
+                break;
+            }
+            if( b == true ) {
+                o++;
+            }
+        }
+        WriteLine( o );
+        return;
+    }
+
+    static IEnumerable<IEnumerable<T>> Permutations<T>( IEnumerable<T> items )
+    {
+        return Permutations( items, items.Count() );
+    }
+
+    static IEnumerable<IEnumerable<T>> Permutations<T>( IEnumerable<T> items, int n )
+    {
+        if( n == 1 ) {
+            foreach( T i in items ) {
+                yield return new T[] { i };
+            }
+        } else {
+            foreach( T i in items ) {
+                IEnumerable<T> u = items.Where( x => x.Equals( i ) != true );
+
+                foreach( IEnumerable<T> j in Permutations( u, n - 1 ) ) {
+                    yield return new T[] { i }.Concat( j );
+                }
+            }
+        }
+    }
+}
