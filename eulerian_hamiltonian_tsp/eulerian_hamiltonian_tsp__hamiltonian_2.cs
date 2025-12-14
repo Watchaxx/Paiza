1cf8bbf5c9140b7c59c86e7928b49f5cc67838ec
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-12-14 20:46:46 +0900

    閉路を n 個増やす2 ハミルトン路の存在判定
    https://paiza.jp/works/mondai/eulerian_hamiltonian_tsp/eulerian_hamiltonian_tsp__hamiltonian_2

---
+++ eulerian_hamiltonian_tsp/eulerian_hamiltonian_tsp__hamiltonian_2.cs
@@ -0,0 +1,30 @@
+// 実行時間 20ms
+using static System.Console;
+using static System.Linq.Enumerable;
+
+class Program
+{
+    static void Main()
+    {
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        bool[][] g = new bool[n[0]][];
+
+        foreach( int i in Range( 0, n[0] ) ) {
+            g[i] = new bool[n[0]];
+        }
+        foreach( int _ in Range( 0, n[1] ) ) {
+            int[] a = ReadLine().Split().Select( x => int.Parse( x ) - 1 ).ToArray();
+
+            g[a[0]][a[1]] = true;
+        }
+
+        int[] v = ReadLine().Split().Select( x => int.Parse( x ) - 1 ).ToArray();
+
+        foreach( int i in Range( 0, n[0] - 1 ).Where( x => g[v[x]][v[x + 1]] != true ) ) {
+            WriteLine( "No" );
+            return;
+        }
+        WriteLine( "Yes" );
+        return;
+    }
+}
