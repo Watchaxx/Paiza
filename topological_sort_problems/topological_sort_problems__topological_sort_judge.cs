a59f4cbc8aa0ccd1abfbe14f1b9c55ee76bc218b
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2025-12-20 16:44:00 +0900

    トポロジカルソート（幅優先探索）3 トポロジカルソート可能かを判定
    https://paiza.jp/works/mondai/topological_sort_problems/topological_sort_problems__topological_sort_judge

---
+++ topological_sort_problems/topological_sort_problems__topological_sort_judge.cs
@@ -0,0 +1,43 @@
+// 実行時間 40ms
+using System.Collections.Generic;
+using static System.Console;
+using static System.Linq.Enumerable;
+
+class Program
+{
+    static void Main()
+    {
+        int f = 0;
+        int[] n = ReadLine().Split().Select( int.Parse ).ToArray();
+        int[] d = new int[n[0]];
+        var g = new List<int>[n[0]];
+        var q = new Queue<int>();
+
+        foreach( int i in Range( 0, n[0] ) ) {
+            g[i] = new List<int>();
+        }
+        foreach( int _ in Range( 0, n[1] ) ) {
+            int[] a = ReadLine().Split().Select( x => int.Parse( x ) - 1 ).ToArray();
+
+            g[a[0]].Add( a[1] );
+            d[a[1]]++;
+        }
+        foreach( int i in Range( 0, n[0] ).Where( x => d[x] == 0 ) ) {
+            f++;
+            q.Enqueue( i );
+        }
+        while( 0 < q.Count ) {
+            int t = q.Dequeue();
+
+            foreach( int i in g[t] ) {
+                d[i]--;
+                if( d[i] == 0 ) {
+                    f++;
+                    q.Enqueue( i );
+                }
+            }
+        }
+        WriteLine( f == n[0] ? "Yes" : "No" );
+        return;
+    }
+}
